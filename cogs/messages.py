82e0d3ffa90c34b29a88ca24680ad7f0bee1e64b
MysteriousDryad <jocelynfellows1@gmail.com>
2023-01-10 13:04:04 -0500

    Embed output for cogs and commands

--- cogs/messages.py
+++ cogs/messages.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands 
 
 
-class Test(commands.Cog):
+class Messages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(name = "hello")
     async def hello(self, ctx):
         await ctx.send(f'Hello!')
     
 async def setup(bot: commands.Bot) -> None:
-   await bot.add_cog(Test(bot))
+   await bot.add_cog(Messages(bot))
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import settings
 import discord 
 from discord.ext import commands
 import cogs 
+import os 
+
 
 logger = settings.logging.getLogger('bot')
 
@@ -14,22 +16,43 @@ async def on_ready():
     guild = discord.utils.get(bot.guilds)
     logger.info(f"{bot.user} is connected to: '{guild.name}' - (ID: {guild.id})")
 
-    await bot.load_extension("cogs.messages")
-
-
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")   
+            
 
 @commands.command()
 @commands.dm_only()
 @commands.is_owner()
 async def sync(ctx):
-    try:
+    try:            
+        
         synced = await bot.tree.sync()
-        await ctx.send(f'Synced {len(synced)} command(s)')
-        print(f'Synced {len(synced)} command(s)')
+        for command in bot.tree.walk_commands():
+            names = f'{command.qualified_name}'
+            title = f"{command.wrapped.module}"
+
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                count = +1
+
+       # ListofCogs = bot.cogs
+
+       # for NameofCog, TheClassofCog in ListofCogs.items():
+           # cog = NameofCog
+
+        emb = discord.Embed(title="Success!", color= discord.Color.green())
+        emb.add_field(name = f"Cogs loaded: {count}", value = title[5:], inline = False)
+        emb.add_field(name = f"Commands: {len(list(synced))}", value = f"{names}")
+        await ctx.send(embed = emb)
+    
+        # await ctx.send(f'``{names}`` synced successfully')
+        # print(f'Synced {len(synced)} command(s)')
     except Exception as e: 
         await ctx.send("Error!")
         print(e)
 bot.add_command(sync)
+
 
 bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
